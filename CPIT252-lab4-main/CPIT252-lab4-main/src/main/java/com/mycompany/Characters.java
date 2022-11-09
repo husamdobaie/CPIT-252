f0713b3b03f4eeeb5c29036413d5d577d9085988
husamdobaie <113587329+husamdobaie@users.noreply.github.com>
2022-11-09 03:43:45 +0300

    Delete CPIT252-lab4-main/CPIT252-lab4-main directory

--- CPIT252-lab4-main/CPIT252-lab4-main/src/main/java/com/mycompany/Characters.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany;
-
-public interface Characters {
-    public String getName();
-    public void setName(String name);
-    public Avatar getAvatar();
-    public void setAvatar(Avatar avatar);
-    public String toString();
-}
